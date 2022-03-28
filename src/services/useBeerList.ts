9b7cd5ac18fba93f68845c8c9078cc3d81114450
dmitry.greblyuk@mail.ru <dmitry.greblyuk@mail.ru>
2022-03-28 05:01:58 +0300

    useBeerList hook added

---
+++ src/services/useBeerList.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { ResponseStatus } from "../types/ResponseStatus";
+import { Beer } from "./Beer";
+
+export const useBeerList = () => {
+  const [requestStatus, setRequestStatus] = React.useState(ResponseStatus.PENDING);
+  const [data, setData] = React.useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await axios("https://api.punkapi.com/v2/beers");
+
+        setRequestStatus(ResponseStatus.SUCCESS);
+        setData(result.data);
+      } catch(error) {
+        console.log(error);
+        setRequestStatus(ResponseStatus.ERROR);
+      }
+    })();
+  }, []);
+
+  return [requestStatus, data] as [ResponseStatus, Beer[]];
+};
