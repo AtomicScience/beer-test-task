e37eca256c42e40b18b56870ef78640c19d05262
dmitry.greblyuk@mail.ru <dmitry.greblyuk@mail.ru>
2022-03-29 12:30:51 +0300

    Added paginator on top + table state retention

--- src/components/BeerTable/index.tsx
+++ src/components/BeerTable/index.tsx
@@ -6,6 +6,7 @@ import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { useBeerListPaginated } from "../../hooks/useBeerListPaginated";
 import { useNavigate } from "react-router-dom";
+import { FavoriteStar } from "./FavoriteStar";
 
 const PAGE_SIZE = 20;
 interface BeerTableProps {
@@ -25,8 +26,11 @@ export const BeerTable: React.FunctionComponent<BeerTableProps> = () => {
       rows={ PAGE_SIZE } 
       first={ first }
       onPage={(e) => setFirst(e.first)}
+      paginatorPosition="both"
+      stateStorage="session" stateKey="dt-state-demo-session"
       onRowClick={(e) => navigate(`/beer/${e.data.id}`)}
     >
+      <Column header="" body={ FavoriteStar }/>
       <Column field="name" header="Name"/>
       <Column field="tagline" header="Tagline"/>
     </DataTable>
