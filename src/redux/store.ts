1616efc6c4a17fbd91b204cb056e6ab80b36547e
dmitry.greblyuk@mail.ru <dmitry.greblyuk@mail.ru>
2022-03-29 15:40:14 +0300

    Local Storage Added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import favoriteSliceReducer from "./favoriteSlice";
 
+const rawPersistedState = localStorage.getItem("favorites");
+/*eslint-disable */
+const persistedState = rawPersistedState
+                       ? JSON.parse(rawPersistedState)
+                       : {};
+/*eslint-enable */
+
 export const store = configureStore({
   reducer: {
     favorite: favoriteSliceReducer,
   },
+  preloadedState: persistedState,
+});
+
+store.subscribe(()=>{
+  localStorage.setItem("favorites", JSON.stringify(store.getState()));
 });
 
 export type RootState   = ReturnType<typeof store.getState>
